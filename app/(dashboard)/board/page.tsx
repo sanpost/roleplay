f6d2975186738820b1eda25b63d482892ccfe1c2
Sandra Postawa <sandrahpostawa@gmail.com>
2024-10-29 12:37:47 +0100

    Added toast errors to profile and cataloge

--- app/(dashboard)/board/page.tsx
+++ app/(dashboard)/board/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import SearchForm from "./_components/form-search";
 import UserList from "./_components/user-list";
 
 export default function CatalogPage() {
   const [users, setUsers] = useState([]);
 
-  const handleSearch = async (searchData: any) => {
+  const handleSearch = async (searchData: any): Promise<any[]> => {
     try {
       const response = await fetch("/api/users/search", {
         method: "POST",
@@ -17,15 +19,28 @@ export default function CatalogPage() {
       });
 
       const data = await response.json();
-      console.log('Found users:', users);
       setUsers(data);
+
+      // Sprawdzenie, czy użytkownicy zostali znalezieni
+      if (!searchData.preferences.length && !searchData.relationships.length && !searchData.ageRanges.length) {
+        toast.info("Nie wybrano kategorii – wyświetlono wszystkich użytkowników.");
+      } else if (data.length > 0) {
+        toast.success(`Znaleziono ${data.length} pasujących użytkowników!`);
+      } else {
+        toast.warn("Brak pasujących użytkowników na podstawie wybranych kategorii.");
+      }
+      
+      return data; // Dodanie zwracania wyników jako tablica
     } catch (error) {
       console.error("Error searching users:", error);
+      toast.error("Wystąpił błąd podczas wyszukiwania. Spróbuj ponownie.");
+      return []; // W przypadku błędu zwracamy pustą tablicę
     }
   };
 
   return (
     <div className="w-full h-full flex flex-col overflow-hidden">
+      <ToastContainer position="bottom-right" />
       <h1 className="text-center text-2xl font-pacifico">Bored? Find someone!</h1>
       <div className="flex flex-row h-full w-full overflow-hidden">
         <div className="xl:w-1/2 w-3/5 flex-shrink-0 overflow-auto">
@@ -33,8 +48,8 @@ export default function CatalogPage() {
             <SearchForm onSearch={handleSearch} />
           </div>
         </div>
-        <div className="xl:w-1/2 w-2/5 h-full overflow-auto">
-          <div className="h-full flex flex-col">
+        <div className="xl:w-1/2 w-2/5 h-full overflow-auto mt-2">
+          <div className="flex flex-col">
             <div className="flex-grow overflow-auto p-6">
               <UserList users={users} />
             </div>
--- app/(dashboard)/panel/_components/profileEdit.tsx
+++ app/(dashboard)/panel/_components/profileEdit.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 interface ProfileEditFormProps {
   email: string;
@@ -85,11 +87,10 @@ const ProfileEditForm: React.FC<ProfileEditFormProps> = ({ email }) => {
     });
 
     if (response.ok) {
-      console.log("Profile updated successfully");
-      // Optionally, show feedback to the user
+      toast.success("Profile updated successfully!");
     } else {
       const errorData = await response.json();
-      console.error("Failed to update profile:", errorData.message);
+      toast.error(`Failed to update profile: ${errorData.message}`);
     }
   };
 
@@ -121,103 +122,106 @@ const ProfileEditForm: React.FC<ProfileEditFormProps> = ({ email }) => {
   );
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex h-auto max-w-full mx-auto p-6 bg-white shadow-md rounded-lg"
-    >
-      <div className="flex flex-col w-1/2 p-4 space-y-4">
-        <div className="h-full overflow-y-auto">
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Username
-            </label>
-            <input
-              id="username"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              required
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="bio"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Bio
-            </label>
-            <textarea
-              id="bio"
-              name="bio"
-              value={formData.bio}
-              onChange={handleChange}
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-              rows={4}
-            />
-          </div>
-          <div>
-            <label
-              htmlFor="age"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Age
-            </label>
-            <input
-              type="number"
-              id="age"
-              name="age"
-              value={formData.age || ""}
-              onChange={handleChange}
-              className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            />
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex h-auto max-w-full mx-auto p-6 bg-white shadow-md rounded-lg"
+      >
+        <div className="flex flex-col w-1/2 p-4 space-y-4">
+          <div className="h-full overflow-y-auto">
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <input
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                required
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="bio"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Bio
+              </label>
+              <textarea
+                id="bio"
+                name="bio"
+                value={formData.bio}
+                onChange={handleChange}
+                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                rows={4}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor="age"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Age
+              </label>
+              <input
+                type="number"
+                id="age"
+                name="age"
+                value={formData.age || ""}
+                onChange={handleChange}
+                className="mt-1 block w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col w-1/2 p-4 space-y-4">
-        <div className="h-full overflow-y-auto">
-          {renderSelect(
-            "Preferences",
-            "preferences",
-            formData.preferences,
-            options.preferencesList
-          )}
-          {renderSelect(
-            "Age Range",
-            "ageRange",
-            formData.ageRange,
-            options.ageRangeList
-          )}
-          {renderSelect(
-            "Relationship",
-            "relationship",
-            formData.relationship,
-            options.relationshipList
-          )}
-          {renderSelect(
-            "Gender",
-            "gender",
-            formData.gender,
-            options.genderList
-          )}
-          {renderSelect(
-            "Contact Method",
-            "contactMethod",
-            formData.contactMethod,
-            options.contactMethodList
-          )}
-          <button
-            type="submit"
-            className="mt-4 w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          >
-            Save Profile
-          </button>
+        <div className="flex flex-col w-1/2 p-4 space-y-4">
+          <div className="h-full overflow-y-auto">
+            {renderSelect(
+              "Preferences",
+              "preferences",
+              formData.preferences,
+              options.preferencesList
+            )}
+            {renderSelect(
+              "Age Range",
+              "ageRange",
+              formData.ageRange,
+              options.ageRangeList
+            )}
+            {renderSelect(
+              "Relationship",
+              "relationship",
+              formData.relationship,
+              options.relationshipList
+            )}
+            {renderSelect(
+              "Gender",
+              "gender",
+              formData.gender,
+              options.genderList
+            )}
+            {renderSelect(
+              "Contact Method",
+              "contactMethod",
+              formData.contactMethod,
+              options.contactMethodList
+            )}
+            <button
+              type="submit"
+              className="mt-4 w-full py-2 px-4 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            >
+              Save Profile
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+      <ToastContainer position="bottom-right" />
+    </>
   );
 };
 
